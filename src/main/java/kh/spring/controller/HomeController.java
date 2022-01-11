4919a56fa89a357d9ee2841074312452b354e950
HoHyun95 <suhoh01@naver.com>
2022-01-11 19:38:13 +0900

    Message Test

--- src/main/java/kh/spring/controller/HomeController.java
+++ src/main/java/kh/spring/controller/HomeController.java
@@ -6,34 +6,40 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * Handles requests for the application home page.
- */
+import kh.spring.dao.MessageDAO;
+import kh.spring.dto.MessageDTO;
+
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+
+	@Autowired
+	public MessageDAO dao;
+
+	@RequestMapping("/") //
+	public String home() {
+
 		return "home";
 	}
-	
+
+	// 입력기능
+//	@RequestMapping("")
+//	public String insertMessage(MessageDTO dto) {
+//
+//		return;
+//	}
+
+	// 입력기능
+//	@RequestMapping("")
+//	public String insertMessage(MessageDTO dto) {
+//
+//		return;
+//	}
+
 }
